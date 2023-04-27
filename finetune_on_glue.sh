ec00c8b763fa233a6c7850a9e6b0ae3ae0a27217
Tianlong Li（李天龙） <72125584+Tengyuantuohai-113@users.noreply.github.com>
2023-04-27 13:10:41 +0800

    Create finetune_on_glue.sh

---
+++ finetune_on_glue.sh
@@ -0,0 +1,11 @@
+python -u finetune_bert_glue.py \
+    --dataset "cola" \
+    --model "bert-base-cased" \
+    --batch_size 64 \
+    --max_length 512 \
+    --lr 5e-4 \
+    --weight_decay 1e-3 \
+    --epochs 10 \
+    --output_dir "./finetuned_model" \
+    --seed 42 > "fine_tune_bert.log"
+
